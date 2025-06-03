8e9ca8a4c01fe83a0f133b5aef1cdd0d7dcfa067
mehdi1413 <mehdifani4@gmail.com>
2025-06-03 16:41:30 +0330

    add breadcrumb templete view

---
+++ template/breadcrumb/breadcrumb.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The breadcrumb template file
+ * @package gifoy
+ */
+defined('ABSPATH') || exit; ?>
+
+<div class="site-breadcrumb rtl">
+    <div class="site-breadcrumb-bg" style="background: url(<?php echo get_template_directory_uri() . '/assets/img/breadcrumb/01.jpg'; ?>)"></div>
+    <div class="container">
+        <div class="site-breadcrumb-wrap">
+            <ul class="breadcrumb-menu">
+                <li><a href="index-2.html"><i class="far fa-home"></i> خانه</a></li>
+                <li class="active">وبلاگ ما</li>
+            </ul>
+        </div>
+    </div>
+</div>
