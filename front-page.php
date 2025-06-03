e01c6e0cc68b97d72e97b694cfb667230d16368a
mehdi1413 <mehdifani4@gmail.com>
2025-06-03 14:49:56 +0330

    use wrapper in fron page

--- front-page.php
+++ front-page.php
@@ -17,24 +17,24 @@ defined('ABSPATH') || exit; ?>
         </form>
     </div>
     <!-- popup search end -->
-    <main class="main">
-        <?php get_template_part('template/front-page/category-section'); ?>
-        <?php get_template_part('template/front-page/hero-section'); ?>
-        <?php get_template_part('template/front-page/feature-section'); ?>
-        <?php get_template_part('template/front-page/product-section'); ?>
-        <?php get_template_part('template/front-page/small-banner-section'); ?>
-        <?php get_template_part('template/front-page/trending-product-section'); ?>
-        <?php get_template_part('template/front-page/video-section'); ?>
-        <?php get_template_part('template/front-page/popular-product-section'); ?>
-        <?php get_template_part('template/front-page/big-banner-section'); ?>
-        <?php get_template_part('template/front-page/fact-section'); ?>
-        <?php get_template_part('template/front-page/deal-section'); ?>
-        <?php get_template_part('template/front-page/about-section'); ?>
-        <?php get_template_part('template/front-page/choose-section'); ?>
-        <?php get_template_part('template/front-page/gallery-section'); ?>
-        <?php get_template_part('template/front-page/testimonial-section'); ?>
-        <?php get_template_part('template/front-page/blog-section'); ?>
-        <?php get_template_part('template/front-page/newsletter-section'); ?>
-        <?php get_template_part('template/front-page/brand-section'); ?>
-    </main>
+<?php get_template_part('template/wrapper/wrapper-start.php'); ?>
+<?php get_template_part('template/front-page/category-section'); ?>
+<?php get_template_part('template/front-page/hero-section'); ?>
+<?php get_template_part('template/front-page/feature-section'); ?>
+<?php get_template_part('template/front-page/product-section'); ?>
+<?php get_template_part('template/front-page/small-banner-section'); ?>
+<?php get_template_part('template/front-page/trending-product-section'); ?>
+<?php get_template_part('template/front-page/video-section'); ?>
+<?php get_template_part('template/front-page/popular-product-section'); ?>
+<?php get_template_part('template/front-page/big-banner-section'); ?>
+<?php get_template_part('template/front-page/fact-section'); ?>
+<?php get_template_part('template/front-page/deal-section'); ?>
+<?php get_template_part('template/front-page/about-section'); ?>
+<?php get_template_part('template/front-page/choose-section'); ?>
+<?php get_template_part('template/front-page/gallery-section'); ?>
+<?php get_template_part('template/front-page/testimonial-section'); ?>
+<?php get_template_part('template/front-page/blog-section'); ?>
+<?php get_template_part('template/front-page/newsletter-section'); ?>
+<?php get_template_part('template/front-page/brand-section'); ?>
+<?php get_template_part('template/wrapper/wrapper-end.php'); ?>
 <?php get_footer(); ?>
