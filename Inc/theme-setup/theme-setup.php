058e69fa8455102e76ca3ae70791f277bd18c78e
mehdi1413 <mehdifani4@gmail.com>
2025-06-03 16:11:29 +0330

    add theme image sizes

--- Inc/theme-setup/theme-setup.php
+++ Inc/theme-setup/theme-setup.php
@@ -10,19 +10,22 @@ function gifoy_theme_setup(): void
     add_theme_support('post-thumbnails');
 
     add_theme_support('title-tag');
-    
-    add_theme_support( 'html5', [
-		'comment-list',
-		'comment-form',
-		'search-form',
-		'gallery',
-		'caption',
-		'style',
-		'script'
-	] );
+
+    add_theme_support('html5', [
+        'comment-list',
+        'comment-form',
+        'search-form',
+        'gallery',
+        'caption',
+        'style',
+        'script'
+    ]);
 
     add_theme_support('woocommerce');
 
+    add_image_size('blog-full', '960', '560', ['center', 'center']);
+    add_image_size('blog-sidebar', '856', '499', ['center', 'center']);
+    add_image_size('blog-card', '374', '250', ['center', 'center']);
 
-	add_filter('show_admin_bar','__return_false');
+    add_filter('show_admin_bar', '__return_false');
 }
