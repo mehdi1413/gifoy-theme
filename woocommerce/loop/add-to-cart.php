09de436b194deef7adc4029a70dce518144c142c
mehdi1413 <mehdifani4@gmail.com>
2025-06-13 11:37:47 +0330

    customize wc add to cart btn structure

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -26,13 +26,12 @@ $aria_describedby = isset( $args['aria-describedby_text'] ) ? sprintf( 'aria-des
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" %s data-quantity="%s" class="%s" %s>%s</a>',
+		'<a href="%s" %s data-quantity="%s" class="product-cart-btn %s" %s><i class="far fa-shopping-bag"></i></a>',
 		esc_url( $product->add_to_cart_url() ),
 		$aria_describedby,
-		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		esc_html( $product->add_to_cart_text() )
+		esc_attr($args['quantity'] ?? 1),
+		esc_attr($args['class'] ?? 'button'),
+		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : ''
 	),
 	$product,
 	$args
