d1968b81c25f0536e11e741a6e6d2e11d655e0fb
mehdi1413 <mehdifani4@gmail.com>
2025-06-13 21:52:12 +0330

    customize single product relateds

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -15,39 +15,36 @@
  * @version     9.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
+$heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
 
-if ( $related_products ) : ?>
+if ($related_products) : ?>
+    <div class="related products product-area related-item pt-40">
+        <div class="container px-0">
+            <div class="row">
+                <?php if ($heading) : ?>
+                    <div class="col-12">
+                        <div class="site-heading-inline rtl">
+                            <span class="site-title"><?php echo esc_html($heading); ?></span>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+        <?php woocommerce_product_loop_start(); ?>
 
-	<section class="related products">
-
-		<?php
-		$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
-
-		if ( $heading ) :
-			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
-		<?php endif; ?>
-		<?php woocommerce_product_loop_start(); ?>
-
-			<?php foreach ( $related_products as $related_product ) : ?>
-
-					<?php
-					$post_object = get_post( $related_product->get_id() );
-
-					setup_postdata( $GLOBALS['post'] = $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
-
-					wc_get_template_part( 'content', 'product' );
-					?>
-
-			<?php endforeach; ?>
-
-		<?php woocommerce_product_loop_end(); ?>
-
-	</section>
-	<?php
+        <?php foreach ($related_products as $related_product) : ?>
+            <?php
+            $post_object = get_post($related_product->get_id());
+            setup_postdata($GLOBALS['post'] = $post_object); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
+            wc_get_template_part('content', 'product');
+            ?>
+        <?php endforeach; ?>
+        <?php woocommerce_product_loop_end(); ?>
+    </div>
+<?php
 endif;
 
 wp_reset_postdata();
