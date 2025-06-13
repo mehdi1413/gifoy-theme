fcd8124ea3cb88a538430b789269387b95681b3d
mehdi1413 <mehdifani4@gmail.com>
2025-06-13 11:39:37 +0330

    change wc loop item title structure - new action

--- functions.php
+++ functions.php
@@ -13,3 +13,12 @@ require_once __DIR__ . '/Inc/manage-assets/register-assets.php';
 
 // ADD CATEGORY META BOX FUNCTION
 require_once __DIR__ . '/class/term-meta/CategoryTermMeta.php';
+
+
+remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title');
+add_action('woocommerce_shop_loop_item_title' , 'gifoy_woocommerce_template_loop_product_title' , 10);
+
+function gifoy_woocommerce_template_loop_product_title(): void
+{
+    echo '<h3 class="product-title"><a href="'. get_the_permalink().'">' . get_the_title() . '</a></h3>';
+}
